144b2aa1b46a93c2c4f847507038fb7b3ef3fda2
DESKTOP-0G6CCDN\ing02 <wenchilin0217@gmail.com>
2021-06-03 15:38:56 +0800

    all

--- unit/framework/vue_demo/app/app.js
+++ unit/framework/vue_demo/app/app.js
@@ -1,2 +1,14 @@
 import Vue from 'vue'
 // Step 1 code here
+/* Step 1
+ * 在 `app.js` 中們新增一個 vue 實例並跟 `#app` 綁定
+ * 根據 vue 作者的說法程式碼中的 h 代表 hyperscript
+ * 我們會在同一個檔案，App.vue，中撰寫 pug/sass/js，並在此引入這個檔案
+ */
+import 'semantic-ui-offline/semantic.min.css'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
--- unit/http/dist/exercise.js
+++ unit/http/dist/exercise.js
@@ -1,15 +1,8 @@
 $(document).ready(function() {
   $('#ajax-form button[type="submit"]').click((event) => {
     event.preventDefault()/*←取消原本預設的功能*/
+  })
 /* Step 9:
- * edit the `$.get()` in the last code snippet with this code snippet
- * edit [element selector]s to appropriate values
- * the second argument of `$.get()` specifies the data sent to server
- * notice the link from html `fname` to server `fname`
- * `fname` in `./dist/exercise.html` vs. `fname` in `./ser.js`
- * packing data explicitly is troublesome, but it brings flexibility
- * open `[host]:[port]/exercise.html` in a browser and open the network tab of the developer console
- * click the ajax submit button and see the request
  * 修改上一個程式碼片段的 `$.get()` ，加上下面的程式碼的內容
  * 修改 `element selector` 成適合的值
  * `$.get()` 的第二個參數，用來將資料送給伺服器
@@ -26,11 +19,6 @@ $.get('./step5', {
   $('ajax-output').html(data)
 })
 /* Step 10:
- * edit the `$.get()` in the code snippet of step 9 with this code snippet
- * the third argument of `$.get()` is a callback function, which is called whenever server responses
- * the first argument of the callback function is the data retruned by server
- * edit [show data in div#ajax-output] to an appropriate
- * open `[host]:[port]/exercise.html` in a browser, click the ajax submit button and see the result
  * 用以下程式碼片段取代 Step 9 中的 `$.get()`
  * `$.get()` 的第三個參數是一個回呼(callback)函式，當伺服器回傳時就會被呼叫
  * 這個回呼函式的第一個參數，就是伺器器回傳的結果
@@ -43,4 +31,4 @@ $.get('./step5', {
     $('#ajax-output').html('loaded')
   }, 100)
   $('#ajax-output').html('loading')    
-})})
+})
